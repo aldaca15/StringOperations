7e010a4acc6c8a76341cd5ef896e2b7f16113aba
Ali Adame <aliadame15@gmail.com>
2019-03-03 15:18:18 -0800

    Update Strings.py
    Changes made based on Cognitive Class lessons of Python for Data Science course.

--- Strings.py
+++ Strings.py
@@ -1,1 +1,17 @@
+# In this example we are going to cover examples for String operations in python
 
+Letters="ABCDEFGHIJK"
+Letters[0:4]
+
+# Substring creation by using slice object
+# contains indices (0, 1, 2, 3)
+sObject = slice(4)
+
+print(Letters[sObject])
+
+# changing the content by assignment
+Letters="WXYZ"
+print(Letters)
+
+# print only YZ
+print(Letters[2:])
